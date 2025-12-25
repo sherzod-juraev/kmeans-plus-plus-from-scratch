36e4c9e1d65e8686edebf6d50a5db0ae4b0de7a2
JuraevSherzod <juraevsherzod9943@gmail.com>
2025-12-25 14:11:53 +0500

    async_engine, Async_Session_Local and Base object wrote in database/connection.py file

---
+++ database/connection.py
@@ -0,0 +1,16 @@
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+from sqlalchemy.orm import declarative_base
+from core import setting
+
+
+async_engine = create_async_engine(
+    url=setting.databse_url,
+    pool_size=40,
+    max_overflow=60,
+    pool_recycle=1800,
+    pool_timeout=3
+)
+
+
+Async_Session_Local = async_sessionmaker(bind=async_engine)
+Base = declarative_base()
