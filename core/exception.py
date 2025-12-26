936192c3b9f30838ed13c3a70f9fb3f91f0597e8
JuraevSherzod <juraevsherzod9943@gmail.com>
2025-12-27 01:11:30 +0500

    value_error_exception_handler function wrote in core/exception.py

--- core/exception.py
+++ core/exception.py
@@ -44,3 +44,16 @@ def register_exception_handler(app: FastAPI, /):
                 'body': exc.errors()
             }
         )
+
+
+    @app.exception_handler(ValueError)
+    async def value_error_exception_handler(
+            request: Request,
+            exc: ValueError
+    ):
+        return JSONResponse(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            content={
+                'detail': str(exc)
+            }
+        )
