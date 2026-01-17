9c1dc54a4c782cb62892afac1139e29e8ab4917f
Sherzod Juraev <juraevsherzod9943@gmail.com>
2026-01-17 16:30:17 +0500

    Conect to logger excention_handler in core/exception.py

--- core/exception.py
+++ core/exception.py
@@ -2,15 +2,38 @@ from fastapi import FastAPI, Request, status
 from fastapi.exceptions import RequestValidationError, ResponseValidationError
 from fastapi.responses import JSONResponse
 from sqlalchemy.exc import TimeoutError
+from .logging import get_logger
+
+
+async def get_ip(request: Request, /) -> str:
+    client_ip = request.headers.get('X-Forwarded-For')
+    if client_ip:
+        client_ip = client_ip.split(',')[0].strip()
+    else:
+        client_ip = request.client.host
+    return client_ip
+
+
+async def get_route_path(request: Request, /) -> str:
+    route_path = request.scope.get('route')
+    if route_path:
+        route_path = getattr(route_path, 'path', request.url.path)
+    else:
+        route_path = request.url.path
+    return route_path
 
 
 def register_exception_handler(app: FastAPI, /):
 
+    logger = get_logger('exception_handler')
     @app.exception_handler(TimeoutError)
     async def timeout_exception_handler(
             request: Request,
             exc: TimeoutError
     ):
+        client_ip = await get_ip(request)
+        route_path = await get_route_path(request)
+        logger.warning(f"{client_ip} {request.method} {route_path} 503 TimeoutError")
         return JSONResponse(
             status_code=status.HTTP_503_SERVICE_UNAVAILABLE,
             content={
@@ -24,6 +47,9 @@ def register_exception_handler(app: FastAPI, /):
             request: Request,
             exc: ResponseValidationError
     ):
+        client_ip = await get_ip(request)
+        route_path = await get_route_path(request)
+        logger.warning(f"{client_ip} {request.method} {route_path} 503 ResponseValidationError")
         return JSONResponse(
             status_code=status.HTTP_503_SERVICE_UNAVAILABLE,
             content={
@@ -37,6 +63,9 @@ def register_exception_handler(app: FastAPI, /):
             request: Request,
             exc: RequestValidationError
     ):
+        client_ip = await get_ip(request)
+        route_path = await get_route_path(request)
+        logger.warning(f"{client_ip} {request.method} {route_path} 400 RequestValidationError")
         return JSONResponse(
             status_code=status.HTTP_400_BAD_REQUEST,
             content={
@@ -51,9 +80,28 @@ def register_exception_handler(app: FastAPI, /):
             request: Request,
             exc: ValueError
     ):
+        client_ip = await get_ip(request)
+        route_path = await get_route_path(request)
+        logger.warning(f"{client_ip} {request.method} {route_path} 503 ValueError")
         return JSONResponse(
             status_code=status.HTTP_400_BAD_REQUEST,
             content={
                 'detail': str(exc)
+            }
+        )
+
+
+    @app.exception_handler(Exception)
+    async def global_exception_handler(
+            request: Request,
+            exc: Exception
+    ):
+        client_ip = await get_ip(request)
+        route_path = await get_route_path(request)
+        logger.error(f"{client_ip} {request.method} {route_path} 500 {exc}")
+        return JSONResponse(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            content={
+                'detail': 'Internal server error'
             }
         )
